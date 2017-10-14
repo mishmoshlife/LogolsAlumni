4da2d99f0114d49533916299f045805404bd585f
mishmoshlife <mwandrei@inthree.com>
2017-10-14 15:17:23 -0400

    Add files via upload

---
+++ LogolsAlumni.Entities/ID.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogolsAlumni.Entities
+{
+    public class InsertID
+    {
+        public int ID { get; set; }
+        public int ud { get; set; }
+    }
+}
