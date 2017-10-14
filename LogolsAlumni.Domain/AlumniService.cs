8274700cd35067bb20d221976260b834351691e1
mishmoshlife <mwandrei@inthree.com>
2017-10-14 15:18:20 -0400

    Add files via upload

---
+++ LogolsAlumni.Domain/AlumniService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LogolsAlumni.DAL;
+using LogolsAlumni.Entities;
+
+namespace LogolsAlumni.Domain
+{
+    public class AlumniService
+    {
+        private AlumniRepository _repository;
+
+        public AlumniService()
+        {
+            _repository = new AlumniRepository();
+        }
+
+        public int Insert(Alumni alumni)
+        {
+            return _repository.Insert(alumni);
+        }
+
+       
+
+        public IEnumerable<Alumni> GetAll()
+        {
+            return _repository.GetAll();
+        }
+
+
+    }
+}
