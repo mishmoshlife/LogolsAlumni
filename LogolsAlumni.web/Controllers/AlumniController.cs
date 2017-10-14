dcc5dce9b34f11abb25ed46e92d669c08eea21e3
mishmoshlife <mwandrei@inthree.com>
2017-10-14 15:25:58 -0400

    Add files via upload

---
+++ LogolsAlumni.web/Controllers/AlumniController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LogolsAlumni.Domain;
+using LogolsAlumni.Entities;
+
+namespace LogolsAlumni.web.Controllers
+{
+    [Route("api/[controller]")]
+    public class AlumniController : Controller
+    {
+        AlumniService _service;
+
+        public AlumniController()
+        {
+            _service = new AlumniService();
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<Alumni> Get()
+        {
+            return _service.GetAll();
+        }
+
+      
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]Alumni alumni)
+        {
+            _service.Insert(alumni);
+        }
+
+       
+
+        
+    }
+}
+
